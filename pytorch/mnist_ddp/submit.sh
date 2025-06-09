45ef916422ab9a10ce5dbbefe5c586ae376f5a71
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2025-06-09 14:25:32 +0800

    Updates mnist template script.

--- pytorch/mnist_ddp/submit.sh
+++ pytorch/mnist_ddp/submit.sh
@@ -11,7 +11,7 @@
 
 module load pytorch/2.2.0-rocm5.7.3
 # The following module swap is needed due to a bug in the PyTorch module.
-module load singularity/3.11.4-nohost
+# module load singularity/3.11.4-nohost
 
 # Number of PyTorch processes per node to be spawned by torchrun.
 # One for each GCD.
@@ -19,18 +19,23 @@ NUM_PYTORCH_PROCESSES=8
 # Setting the number of threads to be generated for each PyTorch process.
 export OMP_NUM_THREADS=8
 
-# The computw node executing the batch script.
+# The compute node executing the batch script.
 export RDZV_HOST=$(hostname)
 export RDZV_PORT=29400                   
 
 # Give all the available resources to torchrun which will then distribute them internally
 # to the processes it creates. Processes are NOT created by srun! For what srun is concerned,
 # only one task is created, the torchrun process.
-
+export MIOPEN_USER_DB_PATH="$MYSCRATCH/my-miopen-cache"
+export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
+mkdir -p /tmp/test-mnist
+cp -r mnist.py data /tmp/test-mnist
+cd /tmp/test-mnist
 srun -c 64 singularity exec  $SINGULARITY_CONTAINER torchrun --nnodes=$SLURM_JOB_NUM_NODES \
     --nproc_per_node=$NUM_PYTORCH_PROCESSES\
     --rdzv_id=$SLURM_JOB_ID \
     --rdzv_backend=c10d \
     --rdzv_endpoint="$RDZV_HOST:$RDZV_PORT" \
     mnist.py --epochs 100
-
+cd -
+rm -rf /tmp/test-mnist
